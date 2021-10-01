a0e6e9ebaab8a5e07a8d51590541c9f4ccca2e83
lpdabest1 <lorenzo.pesina@utexas.edu>
2021-10-01 15:04:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import Passing_Stats_Modern_Era
 import Passing_Stats_Past_Era
+import Rushing_Stats
 
 # Page Configuration
 st.set_page_config(
@@ -16,7 +17,8 @@ st.sidebar.title('Pro Football Statistics')
 
 
 Pages = {"Passing Stats (Modern Era)": Passing_Stats_Modern_Era,
-         "Passing Stats (Past Era)": Passing_Stats_Past_Era
+         "Passing Stats (Past Era)": Passing_Stats_Past_Era,
+         "Rushing Stats": Rushing_Stats
         }
 selection = st.sidebar.selectbox("Select One Of The Following Individual Categories",list(Pages.keys()))
 page = Pages[selection]
